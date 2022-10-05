4c57ccca7f37a564e27675ff73714a3b5e73a9a2
Khadim Gueye <89599484+KhadimGueyeKGY@users.noreply.github.com>
2022-10-05 14:02:53 +0000

    Update Script_sample_29092022.py

--- script/Script_sample_29092022.py
+++ script/Script_sample_29092022.py
@@ -13,7 +13,10 @@ import webbrowser
 
 init1="curl -X GET --header 'Accept: text/xml' 'https://www.ebi.ac.uk/ebisearch/ws/rest/biosamples?filter="
 init2= "https://www.ebi.ac.uk/ebisearch/ws/rest/biosamples?filter="
-end="&size=15" # &fields=id,name,collection_date,TAXONOMY,description,insdc_center_name,geographic_location_country_andor_sea'"
+end="&size=15&fields=id,name,collection_date"
+
+init3 = "https://www.ebi.ac.uk/ebisearch/ws/rest/biosamples?query="
+end3 = "&fields=id,name,collection_date"
 
 '''
 this function allows to read the json file 
@@ -23,18 +26,18 @@ but also to get the taxon_id and to launch the results in the webbrowser.
 def requet (file_directory):
     with open(file_directory) as mf:
         file = json.load(mf)
-    query = init2
+    query = init3
     for i in range(len(file)):
         query += "%20TAXONOMY:"+str(file[i]["taxon_id"])
         if i < len(file)-1:
             query += "%20OR"
-    query += end
+    query += end3
     webbrowser.open_new_tab(query)
 
-# requet('C:/Users/Dell/Desktop/taxon work/hse_pathogens.bacteria_w_taxa.json')
-# requet('C:/Users/Dell/Desktop/taxon work/hse_pathogens.fungi_w_taxa.json')
-# requet('C:/Users/Dell/Desktop/taxon work/hse_pathogens.helminths_w_taxa.json')
-requet('C:/Users/Dell/Desktop/taxon work/hse_pathogens.protozoa_w_taxa.json')
+# requet('C:/Users/Dell/Desktop/taxon work/Colman_Work/input/hse_pathogens.bacteria_w_taxa.json')
+# requet('C:/Users/Dell/Desktop/taxon work/Colman_Work/input/hse_pathogens.fungi_w_taxa.json')
+# requet('C:/Users/Dell/Desktop/taxon work/Colman_Work/input/hse_pathogens.helminths_w_taxa.json')
+requet('C:/Users/Dell/Desktop/taxon work/Colman_Work/input/hse_pathogens.protozoa_w_taxa.json')
 
 
 
